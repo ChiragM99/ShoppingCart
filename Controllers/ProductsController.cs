d35b7d200217793a372771c6902fdb51bac85d0d
ChiragM99 <66064552+ChiragM99@users.noreply.github.com>
2022-02-02 17:20:20 +0000

    Update ProductsController.cs

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -21,56 +21,56 @@ namespace ShoppingCart.Controllers
             return _ProductRepo.GetAll();
         }
 
-        //public Product GetProduct(int id)
-        //{
-        //    Product item = _ProductRepo.Get(id);
-        //    if (item == null)
-        //    {
-        //        throw new HttpResponseException(HttpStatusCode.NotFound);
-        //    }
-        //    return item;
-        //}
+        public Product GetProduct(int id)
+        {
+           Product item = _ProductRepo.Get(id);
+           if (item == null)
+           {
+               throw new HttpResponseException(HttpStatusCode.NotFound);
+           }
+           return item;
+        }
 
-        //public IEnumerable<Product> GetProductsByCategory(string category)
-        //{
-        //    return _ProductRepo.GetAll().Where(
-        //        p => int32.Equals(p.CategoryID, category));
-        //}
+        public IEnumerable<Product> GetProductsByCategory(string category)
+        {
+           return _ProductRepo.GetAll().Where(
+               p => int32.Equals(p.CategoryID, category));
+        }
 
-        //public IEnumerable<Product> GetProductsBySuppler(string supplier)
-        //{
-        //    return _ProductRepo.GetAll().Where(
-        //        p => int32.Equals(p.SupplierID, supplier));
-        //}
+        public IEnumerable<Product> GetProductsBySuppler(string supplier)
+        {
+           return _ProductRepo.GetAll().Where(
+               p => int32.Equals(p.SupplierID, supplier));
+        }
 
-        //public HttpResponseMessage PostProduct(Product item)
-        //{
-        //    item = _ProductRepo.Add(item);
-        //    var response = Request.CreateResponse<Product>(HttpStatusCode.Created, item)
+        public HttpResponseMessage PostProduct(Product item)
+        {
+           item = _ProductRepo.Add(item);
+           var response = Request.CreateResponse<Product>(HttpStatusCode.Created, item)
 
-        //    string uri = Url.Link("DefaultApi", new { id = item.Id });
-        //    response.Headers.Location = new Uri(uri);
-        //    return response;
-        //    //return item;
-        //}
+           string uri = Url.Link("DefaultApi", new { id = item.Id });
+           response.Headers.Location = new Uri(uri);
+           return response;
+           //return item;
+        }
 
-        //public PutProduct(int id, Product product)
-        //{
-        //    product.ProductID = id;
-        //    if (!_ProductRepo.Update(product))
-        //    {
-        //        throw new HttpResponseException(HttpStatusCode.NotFound);
-        //    }
-        //}
+        public PutProduct(int id, Product product)
+        {
+           product.ProductID = id;
+           if (!_ProductRepo.Update(product))
+           {
+               throw new HttpResponseException(HttpStatusCode.NotFound);
+           }
+        }
 
-        //public void DeleteProduct(int id)
-        //{
-        //    Product item = _ProductRepo.Get(id);
-        //    if (item == null)
-        //    {
-        //        throw new HttpResponseException(HttpStatusCode.NotFound);
-        //    }
-        //    _ProductRepo.Remove(id);
-        //}
+        public void DeleteProduct(int id)
+        {
+           Product item = _ProductRepo.Get(id);
+           if (item == null)
+           {
+               throw new HttpResponseException(HttpStatusCode.NotFound);
+           }
+           _ProductRepo.Remove(id);
+        }
     }
 }
